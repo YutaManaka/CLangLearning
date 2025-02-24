cc9ebab407a1675866c58bb075d3598db8ce4b8b
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-02-24 21:44:52 +0900

    計算

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 int
 main(void)
 {
-    printf("hello, world\n");
+    printf("%d\n", 10 + 3);
+    printf("%d\n", 10 - 3);
+    printf("%d\n", 10 * 3);
+    printf("%d\n", 10 / 3); // 端数は切り捨て
+    printf("%d\n", 10 % 3);
+    printf("%d\n", (1 + 100) * 100 / 2);
     return 0;
 }
