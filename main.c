9b9fe871c15f50b27a7b525d0f2f51f754e07f10
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-02-25 21:03:11 +0900

    練習問題5(2-1)

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    int water_price_with_tax, milk_price_with_tax, change;
+    double tax_rate;
+
+    tax_rate = 0.05;
+    water_price_with_tax = (int) (198 * (1 + tax_rate));
+    milk_price_with_tax = (int) (138 * (1 + tax_rate));
+
+    change = 1000 - (water_price_with_tax + milk_price_with_tax * 2);
+
+    printf("%d円\n", change);
     return 0;
 }
