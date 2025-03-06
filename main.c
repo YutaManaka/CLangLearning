0d4b7bb40ce10714bd276aae455ea7a726b8cc2e
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-06 21:43:06 +0900

    break

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+        if (i == 3) break; /* ループを終了する */
+    }
+
     return 0;
 }
