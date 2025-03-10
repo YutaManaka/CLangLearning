85fa8c7f92de1fbd4827169c90dbe2407b927eaf
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-10 20:45:23 +0900

    練習問題10(3-1)

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    int point;
+
+    do {
+        printf("点数？:");
+        scanf("%d", &point);
+    } while (point < 0 || point > 100);
+
+    printf("点数は %03d 点です。\n", point);
+
     return 0;
 }
