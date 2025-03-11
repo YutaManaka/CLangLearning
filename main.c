0b954f6e5a9970c931acfe7354fe95526b9a9232
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-11 21:12:45 +0900

    m

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    char str[] = "MARIO"; // 宣言時に初期化する時のみ、文字列リテラルを使える
+    printf("%s\n", str);
+
+    // 宣言後に代入する場合
+    str[0] = 'R';
+    str[1] = 'U';
+    str[2] = 'I';
+    str[3] = 'J';
+    str[4] = 'I';
+    str[5] = '\0'; // EOS(終端文字)
+    printf("%s\n", str);
+
     return 0;
 }
