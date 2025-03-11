89f963322b18ec622101b4ba557d672027c078c7
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-11 21:46:28 +0900

    練習問題14(3-1)

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    // 文字列の入力、文字数のカウント
+    char last_name[32], first_name[32];
+    printf("苗字を入力してください：");
+    scanf("%32s", last_name);
+    printf("名前を入力してください：");
+    scanf("%32s", first_name);
+
+    // 文字列の連結
+    printf("あなたの名前は%s\n", strcat(last_name, first_name));
+
     return 0;
 }
