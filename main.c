6b4f06b6d5d6fe3ac6b155d2a7605b7e600fdbab
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-12 21:29:59 +0900

    変数とアドレス

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 int
 main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    // int変数のアドレス
+    int i1, i2, i3;
+    printf("i1(%p)\n", &i1);
+    printf("i2(%p)\n", &i2);
+    printf("i3(%p)\n", &i3);
+
+    // 配列のアドレス
+    int array[10];
+    printf("array___(%p)\n", array);
+    printf("array[0](%p)\n", &array[0]);
+    printf("array[1](%p)\n", &array[1]);
+    printf("array[2](%p)\n", &array[2]);
+
     return 0;
 }
