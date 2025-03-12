4b4ff731b0727c0c7343caef7ab430c82d1a1c6f
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-12 22:42:42 +0900

    練習問題15(3-1)

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
-int
-main(void)
+void get_max_and_min(int array[], int *max, int *min);
+
+int main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    int i = 0, num = 0, max = 0, min = 0;
+    int array[10];
+    do {
+        printf("数字を入力してください:");
+        scanf("%d", &num);
+        array[i] = num;
+        i++;
+    } while (num > 0);
+
+    get_max_and_min(array, &max, &min);
+
+    printf("最大値 %d : 最小値 %d\n", max, min);
+
     return 0;
+}
+
+void get_max_and_min(int array[], int *max, int *min)
+{
+    int i = 0;
+
+    *max = 0;
+    *min = 100;
+
+    while (array[i] != -1)
+    {
+        if (array[i] > *max) *max = array[i];
+        if (array[i] < *min) *min = array[i];
+        i++;
+    }
 }
