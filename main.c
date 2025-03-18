42bad6825634a1c612e8f70e093da58b12095842
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-18 21:56:44 +0900

    ドラッグに対応

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int
-main(void)
+int main(int argc, char* argv[])
 {
-    printf("hello, world\nこんにちわ\n");
+    printf("%s\n", argv[0]); // アプリケーション自身のファイル名を表示
+
+    if (argc > 1) {
+        printf("%s\n", argv[1]); // ドラッグされたファイル名を表示
+    }
+
+    fflush(stdin);
+    getchar();
+
     return 0;
 }
