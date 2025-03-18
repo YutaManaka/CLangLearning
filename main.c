610783f6ef0dcd7324432f88e191c79f6c7d55ea
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-03-18 22:14:03 +0900

    定数

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int
-main(void)
+/* ここで定数を宣言 */
+#define TAX_RATE 0.03
+#define AUTHOR "YutaManaka"
+
+int main(void)
 {
-    printf("hello, world\nこんにちわ\n");
+    int price;
+
+    printf("本体価格:");
+    scanf("%d", &price);
+    price = (int)((1 + TAX_RATE) * price); /* 定数使用 */
+    printf("税込価格:%d\n", price);
+
+    printf("作者名:%s\n", AUTHOR); /* 定数使用 */
+
     return 0;
 }
