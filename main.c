66ed40d4b4945f4065603c0fd426a5df534ad7fa
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2025-04-08 21:27:48 +0900

    動的配列

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int
-main(void)
+int main()
 {
-    printf("hello, world\nこんにちわ\n");
+    int i;
+    int* heap;
+    // 動的配列を定義
+    heap = (int*)malloc(sizeof(int) * 10);
+    if (heap == NULL) exit(0);
+
+    for (i = 0; i < 10; i++) {
+        heap[i] = i;
+    }
+
+    printf("%d\n", heap[5]);
+
+    // 動的配列の要素数を変更
+    heap = (int*)realloc(heap, sizeof(int) * 100);
+
+    free(heap);
+
     return 0;
 }
